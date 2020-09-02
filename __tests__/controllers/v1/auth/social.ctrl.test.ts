47ec0fd016b7221246bceb62dcb63d0a0e5f7497
begin0dev <prosonic1@gmail.com>
2020-09-02 22:54:12 +0900

    FIX: test

--- __tests__/controllers/v1/auth/social.ctrl.test.ts
+++ __tests__/controllers/v1/auth/social.ctrl.test.ts
@@ -1,19 +1,13 @@
-import express from 'express';
 import { agent } from 'supertest';
 
-import oAuthStrategies from '@app/middlewares/strategies';
-import socialCtrl from '@app/controllers/v1/auth/social.ctrl';
+import Server from '@app/server';
 
 describe('Test auth social controller', () => {
-  const app = express();
-  oAuthStrategies();
-  app.use('/api/v1/auth', socialCtrl);
-
   test('/facebook', async () => {
-    await agent(app).get('/api/v1/auth/facebook').expect(302);
+    await agent(Server.application).get('/api/v1/auth/social/facebook').expect(302);
   });
 
   test('/kakao', async () => {
-    await agent(app).get('/api/v1/auth/kakao').expect(302);
+    await agent(Server.application).get('/api/v1/auth/social/kakao').expect(302);
   });
 });
