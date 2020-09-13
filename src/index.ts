f46c632b2438ff2035a05f912404243cbbcfe0cd
begin0dev <prosonic1@gmail.com>
2020-09-13 22:28:31 +0900

    FIX: env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
+import 'dotenv/config';
 import 'module-alias/register';
-import dotenv from 'dotenv';
 
 import Server from '@app/server';
-
-dotenv.config();
 
 Server.run(parseInt(process.env.PORT, 10) || 3001);
