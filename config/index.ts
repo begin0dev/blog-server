83cbb8b324ae81351385e63650e1701180f03d57
begin0dev <prosonic1@gmail.com>
2020-09-13 19:35:22 +0900

    FIX: docker

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,4 @@
+import 'module-alias/register';
 import dotenv from 'dotenv';
 
-// eslint-disable-next-line global-require
-if (process.env.NODE_ENV === 'production') require('module-alias/register');
-
 dotenv.config({ path: `./config/.env${process.env.NODE_ENV === 'test' ? '.test' : ''}` });
-
-console.log(process.env)
