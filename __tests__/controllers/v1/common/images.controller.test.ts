81bf37dd01777b77b063acb635cd60a88da2ebcc
begin0dev <prosonic1@gmail.com>
2020-11-08 20:16:53 +0900

    ADD: image test

--- __tests__/controllers/v1/common/images.controller.test.ts
+++ __tests__/controllers/v1/common/images.controller.test.ts
@@ -1,17 +1,39 @@
 import { agent } from 'supertest';
-import { NextFunction, Express } from 'express';
+import { NextFunction } from 'express';
+import faker from 'faker';
 
 import Server from '@app/server';
 
+interface MulterFile {
+  filename: string;
+  fieldname: string;
+  encoding: string;
+  mimetype: string;
+  originalname: string;
+  size: number;
+  path: string;
+}
+
 interface MulterRequest extends Request {
-  files: Express.Multer.File[];
+  files: MulterFile[];
 }
 
 jest.mock('multer', () => () => ({
   any: jest.fn(),
   array() {
     return (req: MulterRequest, res: Response, next: NextFunction) => {
-      req.files = [];
+      const filename = faker.system.commonFileName('png');
+      req.files = [
+        {
+          filename,
+          fieldname: 'images',
+          encoding: '7bit',
+          mimetype: 'image/png',
+          originalname: 'asda',
+          size: 12311,
+          path: faker.image.imageUrl(),
+        },
+      ];
       return next();
     };
   },
